ede9e816d0d965eb192436983b64e13f99f71029
poitras <nicopoitras@gmail.com>
2017-06-28 23:07:52 -0400

    Added async express listener on port 3000

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+var express = require('express');
+var app = express();
+
+app.set('port', 3000);
+
+var server = app.listen(app.get('port'), function () {
+    var port = server.address().port;
+   console.log("Its happening on port: " + port); 
+});
+
--- old/vid4/app.js
+++ old/vid4/app.js
@@ -1,6 +1,6 @@
 require('./instantHello');
 var goodbye = require('./talk/goodbye');
-var talk = require('./talk'); //gonna look first for for a talk file then a folder with a index.js file
+var talk = require('./talk'); //gonna look first for a talk file then a folder with a index.js file
 var question = require('./talk/question');
 
 talk.intro();
