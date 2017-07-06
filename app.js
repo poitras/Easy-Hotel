f6193b911849d99b164c2e570b8282f2e5019c71
poitras <nicopoitras@gmail.com>
2017-07-05 23:50:54 -0400

    Added base route for application

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 var express = require('express');
 var app = express();
+var path = require('path'); // to be able to get html file in a html method call
 
 app.set('port', 3000);
+
+app.get('/', function (req, res) { //req = request, res = response
+    console.log("GET the home page");
+    res
+        .status(200)
+        .sendFile(path.join(__dirname, 'public', 'index.html')); // Les systeme appelant décideront comment monter le path
+});
+
+app.get('/json', function (req, res) {
+    console.log("GET some json");
+    res
+        .status(200)
+        .json({ "jsonData" : true });
+});
+
+app.get('/file', function (req, res) {
+    console.log("GET a html page");
+    res
+        .status(200)
+        .sendFile(path.join(__dirname, 'app.js'));
+});
 
 var server = app.listen(app.get('port'), function () {
     var port = server.address().port;
