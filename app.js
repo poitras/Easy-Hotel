ad79e4c82f1ee2788e7255b77d899f173075d80f
poitras <nicopoitras@gmail.com>
2017-07-07 00:54:43 -0400

    Added route for static file and index.html setup

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ var path = require('path'); // to be able to get html file in a html method call
 
 app.set('port', 3000);
 
-app.get('/', function (req, res) { //req = request, res = response
+app.use(express.static(path.join(__dirname, 'public'))); // Will go directly in public folder and search for html
+
+//app.use('/public', express.static(path.join(__dirname, "public"))); //to use path like ../public/index.html
+
+/*app.get('/', function (req, res) { //req = request, res = response
     console.log("GET the home page");
     res
         .status(200)
         .sendFile(path.join(__dirname, 'public', 'index.html')); // Les systeme appelant décideront comment monter le path
-});
+});*/
 
 app.get('/json', function (req, res) {
     console.log("GET some json");
@@ -29,4 +33,3 @@ var server = app.listen(app.get('port'), function () {
     var port = server.address().port;
    console.log("Its happening on port: " + port); 
 });
-
