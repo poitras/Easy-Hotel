00c69521a9b437dca06cb3a52cef9e4dfa6083b8
poitras <nicopoitras@gmail.com>
2017-07-09 16:04:51 -0400

    Ajout fichier route api

--- app.js
+++ app.js
@@ -2,32 +2,18 @@ var express = require('express');
 var app = express();
 var path = require('path'); // to be able to get html file in a html method call
 
+var routes = require('./routes');
+
 app.set('port', 3000);
+
+app.use(function (req, res, next) {    // utilisation du middleware pour afficher les info concernant le requète doit être avant le call static car il descent le fichier
+    console.log(req.method, req.url);
+    next();   
+});
 
 app.use(express.static(path.join(__dirname, 'public'))); // Will go directly in public folder and search for html
 
-//app.use('/public', express.static(path.join(__dirname, "public"))); //to use path like ../public/index.html
-
-/*app.get('/', function (req, res) { //req = request, res = response
-    console.log("GET the home page");
-    res
-        .status(200)
-        .sendFile(path.join(__dirname, 'public', 'index.html')); // Les systeme appelant décideront comment monter le path
-});*/
-
-app.get('/json', function (req, res) {
-    console.log("GET some json");
-    res
-        .status(200)
-        .json({ "jsonData" : true });
-});
-
-app.get('/file', function (req, res) {
-    console.log("GET a html page");
-    res
-        .status(200)
-        .sendFile(path.join(__dirname, 'app.js'));
-});
+app.use('/api', routes); // will look in the routes folder for any routes avec / but could be use like '/api'
 
 var server = app.listen(app.get('port'), function () {
     var port = server.address().port;
