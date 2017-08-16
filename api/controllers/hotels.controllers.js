ef34e07b7d981bb1229ed6b4130aa9142ae47a3b
poitras <nicopoitras@gmail.com>
2017-08-15 23:51:01 -0400

    hotels and hotels/:id working with mongo

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -1,43 +1,56 @@
 var dbconn = require('../data/dbconnection');
+// object id helper to find by id
+var ObjectId = require('mongodb').ObjectId;
 var hotelData = require('../data/hotel-data.json'); // create json data array
 
 module.exports.hotelsGetAll = function (req, res) {
 
     // call everytime to get a new one
     var db = dbconn.get();
-    
-    console.log("GET the Hotels");
-    console.log(req.query);
-    // to use with params like /hotels?offset=2&count=2
-    var data = hotelData;
-    if (req.query) {
-        var offset = 0;
-        var count = 5;
+    // we cache the collection
+    var collection = db.collection('hotels');
 
-        if (req.query && req.query.offset) {
-            offset = parseInt(req.query.offset, 10);
-        }
-        if (req.query && req.query.count) {
-            count = parseInt(req.query.count, 10);
-        }
-        data = hotelData.slice(offset, offset+count);   
+    // to use with params like /hotels?offset=2&count=2
+    var offset = 0;
+    var count = 5;
+
+    if (req.query && req.query.offset) {
+        offset = parseInt(req.query.offset, 10);
+    }
+    if (req.query && req.query.count) {
+        count = parseInt(req.query.count, 10);
     }
 
-    res
-        .status(200)
-        .json( data );
+    // use the collection to return a array + in asynchronus none blocking way with toArray();
+    // skip for when to start and limit for the number of docs we want
+    collection
+        .find()
+        .skip(offset)
+        .limit(count)
+        .toArray(function(err, docs) {
+            //console.log('Founds hotels', docs);
+            res
+                .status(200)
+                .json(docs);
+        });
 };
 
 module.exports.hotelsGetOne = function (req, res) {
 
     var db = dbconn.get();
+    var collection = db.collection('hotels');
 
     var hotelId = req.params.hotelId;
-    var thisHotel = hotelData[ hotelId ]; // get data at position "hotelId" in hotelData array 
-    console.log("GET hotel with id: " + hotelId);
-    res
-        .status(200)
-        .json( thisHotel );
+    console.log("GET hotel with id", hotelId);
+
+    collection
+        .findOne({
+            _id : ObjectId(hotelId)
+        }, function(err, docs) {
+            res
+                .status(200)
+                .json( docs );
+        });
 };
 
 module.exports.hotelsAddOne = function (req, res) {
