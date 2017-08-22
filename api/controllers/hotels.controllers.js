9dfbc8d78cf993fb1ade5793ac8b961f21af07bd
poitras <nicopoitras@gmail.com>
2017-08-22 11:37:51 -0400

    Added post and put for hotels and reviews

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -7,6 +7,14 @@ var runGeoQuery = function(req, res) {
     var lng = parseFloat(req.query.lng);
     var lat = parseFloat(req.query.lat);
 
+    if (isNaN(lng) || isNaN(lat)) {
+        res
+            .status(400)
+            .json({
+                "message" : "If supplied in querystring, lng and lat must both be numbers"
+            });
+        return;
+    }
     // A geoJSON point
     var point = {
         type : "Point",
@@ -22,16 +30,24 @@ var runGeoQuery = function(req, res) {
         .geoNear(point, geoOptions, function(err, results, stats) {
             console.log('Geo results', results);
             console.log('Geo stat', stats);
-            res
-                .status(200)
-                .json(results);           
+            if (err) {
+                console.log("Error finding hotels");
+                res
+                    .status(500)
+                    .json(err);
+            } else {
+                res
+                    .status(200)
+                    .json(results);
+            }           
         });
 };
 module.exports.hotelsGetAll = function (req, res) {
 
     // to use with params like /hotels?offset=2&count=2
     var offset = 0;
-    var count = 10;
+    var count = 5;
+    var maxCount = 10;
 
     if (req.query && req.query.lat && req.query.lng) {
         runGeoQuery(req, res);
@@ -44,15 +60,36 @@ module.exports.hotelsGetAll = function (req, res) {
     if (req.query && req.query.count) {
         count = parseInt(req.query.count, 10);
     }
+    // Validation
+    if (isNaN(offset) || isNaN(count)) {
+        res
+            .status(400)
+            .json({ "message" : "If supplied in querystring count and offset should be numbers"});
+        return;
+    }
+    if (count > maxCount) {
+        res
+            .status(400)
+            .json({ "message" : "Count limit of " + maxCount + " exceeded" });
+        return;
+    }
 
     Hotel
         .find()
         .skip(offset)
         .limit(count)
         .exec(function(err, hotels) {
-            console.log('Found hotels', hotels.length);
-            res
-                .json(hotels);           
+            if (err) {
+                console.log('Error finding hotels');
+                res
+                    .status(500)
+                    .json(err);
+            } else {
+                console.log('Found hotels', hotels.length);
+                res
+                    .status(200)
+                    .json(hotels);   
+            }        
         });
 };
 
@@ -63,37 +100,114 @@ module.exports.hotelsGetOne = function (req, res) {
     Hotel
         .findById(hotelId)
         .exec(function(err, hotel) {
+            var response = {
+                status : 200,
+                message : hotel
+            };
+            if (err) {
+                console.log('Error finding hotels');
+                response.status = 500;
+                response.message = err;
+            } else if (!hotel) {
+                response.status = 404;
+                response.message = { "message" : "Hotel ID not found" };
+            }
             res
-                .status(200)
-                .json( hotel );
+                .status(response.status)
+                .json( response.message );
         });
+};
+
+var _splitArray = function(input) {
+    var output;
+    if (input && input.length > 0) {
+        output = input.split(";");
+    } else {
+        output = [];
+    }
+    return output;
 };
 
 module.exports.hotelsAddOne = function (req, res) {
-
-    var db = dbconn.get();
-    var collection = db.collection('hotels');
-    // to parse the data from the request and make it accepted data
-    var newHotel;
-
-    console.log("POST new hotel");
-
-    if (req.body && req.body.name && req.body.stars) {
-
-        var newHotel = req.body;
-        newHotel.stars = parseInt(req.body.stars, 10);
-        
-        collection.insertOne(newHotel, function(err, response) {
-            console.log(response.ops);
-            res
-                .status(201)
-                .json( response.ops );
+    Hotel
+        .create({
+            name : req.body.name,
+            description : req.body.description,
+            stars : parseInt(req.body.stars, 10),
+            services : _splitArray(req.body.services),
+            photos : _splitArray(req.body.photos),
+            currency : req.body.currency,
+            location : {
+                address : req.body.address,
+                coordinates : [
+                    parseFloat(req.body.lng), 
+                    parseFloat(req.body.lat)
+                ]
+            }
+        }, function(err, hotel) {
+            if (err) {
+                console.log('Error creating hotel');
+                res
+                    .status(400)
+                    .json(err);
+            } else {
+                console.log('Hotel created', hotel);
+                res
+                    .status(201)
+                    .json( hotel );                
+            }
         });
-
-    } else {
-        console.log('Data missing from body');
-        res
-            .status(400)
-            .json({message : "Required data missing from body"});    
-    }
 };
+
+module.exports.hotelsUpdateOne = function(req, res) {
+    var hotelId = req.params.hotelId;
+    console.log("GET hotel with id", hotelId);
+
+    Hotel
+        .findById(hotelId)
+        .select("-reviews -rooms")
+        .exec(function(err, hotel) {
+            var response = {
+                status : 200,
+                message : hotel
+            };
+            if (err) {
+                console.log('Error finding hotels');
+                response.status = 500;
+                response.message = err;
+            } else if (!hotel) {
+                response.status = 404;
+                response.message = { "message" : "Hotel ID not found" };
+            }
+            if(response.status !== 200) {
+                res
+                    .status(response.status)
+                    .json( response.message );
+            } else {
+                hotel.name = req.body.name,
+                hotel.description = req.body.description,
+                hotel.stars = parseInt(req.body.stars, 10),
+                hotel.services = _splitArray(req.body.services),
+                hotel.photos = _splitArray(req.body.photos),
+                hotel.currency = req.body.currency,
+                hotel.location = {
+                    address : req.body.address,
+                    coordinates : [
+                        parseFloat(req.body.lng), 
+                        parseFloat(req.body.lat)
+                    ]
+                }
+                hotel.save(function(err, updatedHotel) {
+                    if (err) {
+                        res
+                            .status(500)
+                            .json(err);
+                    } else {
+                        res
+                            .status(204)
+                            .json();
+                    }
+                });
+            }
+        });
+}
--- api/routes/index.js
+++ api/routes/index.js
@@ -6,20 +6,21 @@ var ctrlReviews = require('../controllers/reviews.controllers');
 
 router
     .route('/hotels')
-    .get( ctrlHotels.hotelsGetAll );
+    .get( ctrlHotels.hotelsGetAll )
+    .post( ctrlHotels.hotelsAddOne );
 router
     .route('/hotels/:hotelId')
-    .get( ctrlHotels.hotelsGetOne );
-router
-    .route('/hotels/new')
-    .post( ctrlHotels.hotelsAddOne );
+    .get( ctrlHotels.hotelsGetOne )
+    .put( ctrlHotels.hotelsUpdateOne );
 
 // Review routes
 router
     .route('/hotels/:hotelId/reviews')
-    .get( ctrlReviews.reviewsGetAll );
+    .get( ctrlReviews.reviewsGetAll )
+    .post( ctrlReviews.reviewsAddOne );
 router
     .route('/hotels/:hotelId/reviews/:reviewId')
-    .get( ctrlReviews.reviewsGetOne );
+    .get( ctrlReviews.reviewsGetOne )
+    .put( ctrlReviews.reviewsUpdateOne );
 
 module.exports = router;
